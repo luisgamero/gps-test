07cfc8a6ac798a5000987dc3a58d587793e7270e
luisgamero <gamero.luis.enrique@gmail.com>
2016-01-21 18:22:16 -0800

    fixed heroku port

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ io.on('connection', function(socket){
   })
 });
 
-http.listen(8000, function(){
-  console.log('listening on port 8000');
+var port = process.env.PORT || 5000;
+
+http.listen(port, function(){
+  console.log('listening on port: ', port);
 });
